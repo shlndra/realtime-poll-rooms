9a457ddc94d45b3de21d509bfb22a3f2db9df9c0
shailender <sk023048@gmail.com>
2026-02-16 02:10:22 +0530

    Fix deployment port configuration

--- app.py
+++ app.py
@@ -88,7 +88,11 @@ def vote():
 
     return redirect(url_for('poll_page', poll_id=poll_id))
 
+import os
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True, port=5005)
+    port = int(os.environ.get("PORT", 10000))
+    socketio.run(app, host="0.0.0.0", port=port)
+
